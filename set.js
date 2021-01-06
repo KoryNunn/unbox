132421d138f5298020d0a64ab0fd1b9fc0ab868f
arjunshibu <arjunshibu1999@gmail.com>
2021-01-06 22:51:13 +0530

    Security fix for Prototype Pollution

--- set.js
+++ set.js
@@ -2,16 +2,16 @@ module.exports = function set(reference, pathParts, value) {
     if(typeof pathParts === 'string'){
         pathParts = pathParts.split('.');
     }
-
+    
     var index = 0,
-        pathLength = pathParts.length,
-        result = reference,
-        previousresult,
-        previousKey;
-
+    pathLength = pathParts.length,
+    result = reference,
+    previousresult,
+    previousKey;
+    
     for(; index < pathLength; index++){
         var key = pathParts[index];
-
+        
         if ((typeof result !== 'object' || result === null) && index < pathLength) {
             if (typeof key !== 'symbol' && !Number.isNaN(Number(key))) {
                 result = previousresult[previousKey] = [];
@@ -24,9 +24,14 @@ module.exports = function set(reference, pathParts, value) {
             result[key] = value;
         }
         else {
+            if (isPrototypePolluted(key)) continue;
             previousresult = result;
             previousKey = key;
             result = result[key];
         }
     }
 };
+
+function isPrototypePolluted(key) {
+    return ['__proto__', 'constructor', 'prototype'].includes(key);
+}
