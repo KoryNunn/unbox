b353b422ec1dd607cbc3db274cf40c85cc953874
Arjun Shibu <arjunshibu1999@gmail.com>
2021-01-12 07:47:07 +0530

    Update set.js

--- set.js
+++ set.js
@@ -2,16 +2,16 @@ module.exports = function set(reference, pathParts, value) {
     if(typeof pathParts === 'string'){
         pathParts = pathParts.split('.');
     }
-    
+
     var index = 0,
     pathLength = pathParts.length,
     result = reference,
     previousresult,
     previousKey;
-    
+
     for(; index < pathLength; index++){
         var key = pathParts[index];
-        
+
         if ((typeof result !== 'object' || result === null) && index < pathLength) {
             if (typeof key !== 'symbol' && !Number.isNaN(Number(key))) {
                 result = previousresult[previousKey] = [];
